f31901d3929a43ac8e42f40428ed1adf0c0ad3f0
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-04-19 17:37:07 +0000

    Add files via upload

---
+++ onebest.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rate 144_00 --rotate normal --output DVI-D-1 --off
